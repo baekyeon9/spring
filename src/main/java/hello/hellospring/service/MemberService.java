4c55069e8b21187edae3691f78185a4d0622f1a7
baekyeon9 <dev.baekyeon@gmail.com>
2022-01-11 23:58:11 +0900

    22/01/11

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,19 +2,16 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -22,8 +19,6 @@ public class MemberService {
     /**
      * 회원가입
      */
-
-
     public Long join(Member member) {
 
         validateDuplicateMember(member); // 중복 회원 검증
